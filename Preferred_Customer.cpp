08d2a9127bfe2983360d3bde10ea55010248211d
Vaughn-Alston <vaughn2code@gmail.com>
2025-04-03 00:19:54 -0700

    Libaries

---
+++ Preferred_Customer.cpp
@@ -0,0 +1,13 @@
+//Programmer Vaughn Alston
+// Weel 4 HW, PreferredCustomer
+//Version 1.0
+// // A retail store has a preferred customer plan where customers may earn discounts on
+// all their purchases. The amount of a customer’s discount is determined by the amount
+// of the customer’s cumulative purchases in the store.
+// //The purchaseAmount variable holds the total of a customer’s purchases to date. The
+// discountLevel variable should be set to the correct discount percentage, according to
+// the store’s preferred customer plan. Write appropriate member functions for this class
+// and demonstrate it in a simple program.
+
+#include<iostream>
+using namespace std;
